93628454fe1dbdbb847ef9d1676924c94af66e44
jiancamposano <80659572+jiancamposano@users.noreply.github.com>
2024-02-10 05:32:21 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -119,7 +119,7 @@ function App() {
           <div className="flex justify-center items-center flex-col space-y-10">
             <h1 className="text-4xl text-white font-bold">Sabia que dirias que si ❤️!</h1>
             <img src="https://i.pinimg.com/originals/9b/dc/c6/9bdcc6206c1d36a37149d31108c6bb41.gif" alt="" className="mx-auto" />
-            <span hidden>{document.title = 'Sabia que dirias que si ❤️!'}</span>
+            <span hidden>{document.title = 'Sabia que dirias que si =) ❤️ TE AMO ABIGAIL ❤️!'}</span>
           </div>
         )
       }
